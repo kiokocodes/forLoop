712d428b4ad7e9744e6bf5194f6b54fe1344994a
Joseph Kioko <kiokocodes@gmail.com>
2022-03-25 03:24:21 +0300

    worked on data types and used the ES6 Template string

--- booleanundundefined.js
+++ booleanundundefined.js
@@ -1,11 +1,29 @@
-const meinemAlter = 33;
-const meinName ="Kioko";
-const meinHobby = "Coding";
-const mochteAnNicht = "Moskitos";
+const blogArticles = "websites, digital Marketing, ecommerce, designAndBranding";
+
+console.log(blogArticles.toUpperCase());
+console.log(blogArticles.substring(29, 38).toUpperCase());
 
 
+//String
+const surname = "kioko";
 
+//Boolean
+const isCool = true;
 
+//Integers/Numbers
+const age = 33;
 
+//Undefined
+const residence = undefined;
 
+//Null
+const maritalStatus = null;
 
+let x;
+
+console.log(surname.toUpperCase());
+console.log(isCool);
+console.log(age);
+console.log(residence);
+console.log(maritalStatus);
+console.log(x);
