7815de7ba8e92538c3cb2103b7607667d88b4227
Joseph Kioko <kiokocodes@gmail.com>
2022-03-31 03:15:21 +0300

    commits for .j file and .html with further MDN code samples

---
+++ arraysEins.js
@@ -0,0 +1,42 @@
+//MDN Network code samples
+//Using arrays to tabulate a set of values
+/*const values = [];
+for (let x = 0; x < 10; x++) {
+  values.push([
+    2 ** x,
+    2 * x ** 2,
+  ]);
+}
+console.table(values);
+
+const fruhstuckenMachen = [];
+for (let hungrisch = 0; hungrisch < 10; hungrisch++) {
+    fruhstuckenMachen.push( [ 10 ** hungrisch, 10 * hungrisch ** 7,])
+}
+console.table(fruhstuckenMachen); */
+
+
+//Machen sie viele arrays
+
+/* const randomArr = ["joseph", "kioko", "kiamba",33, true];
+console.log(randomArr.length);
+console.table(randomArr);
+console.warn(randomArr);
+console.log(randomArr);
+console.error(randomArr);
+
+*/
+
+//Example 2 of Arrays
+const kiokoCodes = ["JavaScript","PHP","ReactJS","mongoDB","Python",];
+console.log(kiokoCodes);
+console.table(kiokoCodes); //Ich mag diese ;)
+
+    // Create a string from an array
+    const kiokoStrings = kiokoCodes.join(", ");
+    console.log(kiokoStrings);
+
+    // Append an item to an array
+    const newLength = kiokoCodes.push('TypeScript');
+    console.log(kiokoCodes); //check whether new item has beenadded to array
+    console.log(newLength); //Check length of array after addition of item
