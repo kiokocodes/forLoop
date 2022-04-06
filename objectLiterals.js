a20d068af966b95bd17f65b43dd13fa641f31df5
Joseph Kioko <kiokocodes@gmail.com>
2022-04-06 03:17:54 +0300

    made changes to the .js syntax and worked on a couple of Object literal examples too.

--- objectLiterals.js
+++ objectLiterals.js
@@ -1,34 +1,6 @@
-//MDN Network code samples
-//Using arrays to tabulate a set of values
-/*const values = [];
-for (let x = 0; x < 10; x++) {
-  values.push([
-    2 ** x,
-    2 * x ** 2,
-  ]);
-}
-console.table(values);
-
-const fruhstuckenMachen = [];
-for (let hungrisch = 0; hungrisch < 10; hungrisch++) {
-    fruhstuckenMachen.push( [ 10 ** hungrisch, 10 * hungrisch ** 7,])
-}
-console.table(fruhstuckenMachen); */
-
-
-//Machen sie viele arrays
-
-/* const randomArr = ["joseph", "kioko", "kiamba",33, true];
-console.log(randomArr.length);
-console.table(randomArr);
-console.warn(randomArr);
-console.log(randomArr);
-console.error(randomArr);
-
-*/
 
 //Example 2 of Arrays
-const kiokoCodes = ["JavaScript","PHP","ReactJS","mongoDB","Python",];
+/* const kiokoCodes = ["JavaScript","PHP","ReactJS","mongoDB","Python",];
 console.log(kiokoCodes);
 console.table(kiokoCodes); //Ich mag diese ;)
 
@@ -62,3 +34,69 @@ console.table(kiokoCodes); //Ich mag diese ;)
       }
     }
     console.table(personen);
+    */
+
+    
+
+   /* //Object Literals
+
+    const person = {
+      firstName: 'Joseph',
+      lastName: 'Kioko',
+      age: 33,
+      hobbies: ['musikHoren','Kino', 'wandert', 'kochen'],
+      address: {
+        street: '50 Main Street',
+        city: 'Mombasa',
+        state: 'Coast'
+      }
+    }
+    */
+
+   // console.table(person); // tabulate the object
+   // console.log(person.firstName); //Value access with .DOT Notation
+  //  console.log(person.firstName, person.lastName);
+
+  //  console.log(person.hobbies[1]); //Accessing arrays within an Object
+
+  //DESTRUCTURING - New to ES6.
+  
+  //const { firstName, lastName, address: {city} } = person;
+
+//  console.log(firstName); //Accessing firstName value
+ // console.log(city);      //Accessing city value
+
+ //person.email = 'joseph.kkiamba@gmail.com'; //Adding a data point to Object.
+ // console.log(person.email);
+
+
+ //ARRAYS OF OBJECTS
+
+ const todos = [
+   {
+     id: 1,
+     text: 'Eat Out',
+     isCompleted: true
+   },
+   {
+    id: 1,
+    text: 'Do Laundry',
+    isCOmpleted: true
+  },
+  {
+    id: 1,
+    text: 'Code Some More',
+    isCompleted: false
+  }
+ ];
+
+ console.table(todos);
+ console.log(todos[1].text)
+ console.log(todos[2].isCompleted);
+
+
+
+  // JSON - Used alot within APIs when sending data to servers
+  
+  const todoJSON = JSON.stringify(todos);
+  console.log(todoJSON);
